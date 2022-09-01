9a5fabde1bb12b71d5bbca73e801767c3a44e92c
Azizbek Kobilov <kobilov.2000@mail.ru>
2022-09-02 00:23:45 +0500

    fixed

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -4,12 +4,12 @@ public class NamingConvention {
     public static void main(String[] args) {
         int _variable = 10;
         int variable_one = 1;
-        int variable_two = 2;
+        int variableTwo = 2;
         int variable_three = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
-        int variableEight = 8;
+        int VariableEight = 8;
     }
 }
